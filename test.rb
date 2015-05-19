df37199ef364dfff0d691c3190bc2e8143c84b06
chrislabarge <chrislabarge2@gmail.com>
2015-05-19 10:38:25 -0400

    completed the computer ai, just need to refactor.

--- test.rb
+++ test.rb
@@ -3,4 +3,132 @@ require './tic_tac_toe.rb'
 
 class Tc_MyTest < Test::Unit::TestCase
 
+	def setup
+		row_1 = [1,2,3]
+		row_2 = [4,5,6]
+		row_3 = [7,8,9]
+
+		@board = [row_1, row_2, row_3]
+
+	end
+
+	def test_diagonals
+		assert_equal [[1,5,9],[3,5,7]], diagonals
+	end
+
+	def test_columns
+		assert_equal [[1,4,7],[2,5,8],[3,6,9]], columns
+	end
+
+	def test_rows
+		assert_equal [[1,2,3],[4,5,6],[7,8,9]], rows
+	end
+
+	def test_ai_middle
+		@board = [['X',2,3],[4,5,6],[7,8,9]]
+		ai(1)
+		assert_equal [['X',2,3],[4,'O',6],[7,8,9]], @board,
+		"AI should choose middle on its turn if player has not"
+		
+	
+	end
+	
+
+	def test_ai_rows
+		#test ai row top
+		@board = [['X','X', 3],[4,'O',6],[7,8,9]]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [['X','X','O'],[4,'O',6],[7,8,9]], @board,
+		"The AI's move to block player winning top row left to right is not working"
+
+		@board = [[1,2,3],[4,'O',6],[7,'X','X']]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [[1,2,3],[4,'O',6],['O','X','X']], @board, 
+		"The AI's should block player winning top row right to left"
+
+		@board = [[1,2,3],[4,'O',6],['X','X',9]]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [[1,2,3],[4,'O',6],['X','X','O']], @board,
+		"The AI should block player winning on bottom row, left to right"
+
+		@board = [[1,2,3],[4,'O',6],[7,'X','X']]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [[1,2,3],[4,'O',6],['O','X','X']], @board,
+		"The AI should block player winning on bottom row, right to left"
+
+	end
+
+	def test_ai_columns
+
+		@board = [[1,2,'X'],[4,'O','X'],[7,8,9]]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [[1,2,'X'],[4,'O','X'],[7,8,'O']], @board,
+		"The AI should block player winnning on right column top to bottom"
+
+		@board = [[1,2,3],[4,'O','X'],[7,8,'X']]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [[1,2,'O'],[4,'O','X'],[7,8,'X']], @board
+		"The AI should block player winnning on right column, bottom to top"
+
+		@board = [['X',2,3],['X','O',6],[7,8,9]]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [['X',2,3],['X','O',6],['O',8,9]], @board,
+		"The AI should block player winnning on left column top to bottom"
+
+		@board = [[1,2,3],['X','O',6],['X',8,9]]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [['O',2,3],['X','O',6],['X',8,9]], @board,
+		"The AI should block player winnning on left column, bottom to top"
+
+
+	end
+
+
+	def test_ai_diagonls
+		@board = [[1,2,'X'],[4,'X','O'],[7,8,9]]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [[1,2,'X'],[4,'X','O'],['O',8,9]], @board,
+		"The AI should block player winnning on diagonal top right to bottom left"
+
+		@board = [[1,2,3],[4,'X','O'],['X',8,9]]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [[1,2,'O'],[4,'X','O'],['X',8,9]], @board,
+		"The AI should block player winnning on diagonal bottom left to top right"
+
+		@board = [[1,2,3],[4,'X','O'],[7,8,'X']]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [['O',2,3],[4,'X','O'],[7,8,'X']], @board,
+		"The AI should block player winnning on diagonal bottom right to top left"
+
+		@board = [['X',2,3],[4,'X','O'],[7,8,9]]
+		ai(2) #this parameter just has to be greater then 1
+		assert_equal  [['X',2,3],[4,'X','O'],[7,8,'O']], @board,
+		"The AI should block player winnning on diagonal top left to bottom right"
+	end
+
+
+	def test_computer_move
+		assert_equal [[1,2,'O'],[4,5,6],[7,8,9]], computer_move(3)
+		assert_equal [['O',2,'O'],[4,5,6],[7,8,9]], computer_move(1)
+	end
+
+
+
+	def test_ai
+
+	
+		
+
+		#this test row is not working
+		
+		
+		#this column test is not working
+	#	@board = [[1,2,3],[4,'O','X'],[7,8,'X']]
+	#	ai(2) #this parameter just has to be greater then 1
+	#	assert_equal  [[1,2,'O'],[4,'O','X'],[7,8,'X']], @board
+
+
+
+	end
+
 end	
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,6 +6,18 @@ row_3 = [7,8,9]
 
 
 
+def count(line)
+	line
+end
+
+def available_spaces # this is a good idea but i dont know if it works
+	available = []
+	@board.each do |row|
+		available << row.select { |x| x != "X" && x != "O" }
+	end
+	available 
+end
+
 
 def board_layout(row)
 	#line = "-----|-----|-----"
@@ -46,4 +58,143 @@ def show_board
 end
 
 show_board
+
+
+def ai(turn)
+	
+	#if @board [1][1] == 5 && turn == 1 #need to figure this out
+ 	#	computer_move(5) 
+  if @board [1][1] != 5 && turn == 1 
+  	computer_move [1,3,7,9].sample
+  elsif	@board [1][1] == 5 && turn == 1
+		computer_move(5)  
+  end
+
+  computer_move(where_is_2) if turn > 1
+
+
+end
+
+
+def computer_move(move)
+	@board.each do |row| 
+		row.collect! do |position|
+			if position == move 
+					"O"
+				else
+					position
+			end		
+		end
+	end	
+end
+
+
+def player_move(move)
+	@board.each do |row| 
+		row.collect! do |position|
+			if position == move 
+					"X"
+				else
+					position
+			end		
+		end
+	end	
+end
+
+def win?
+	win = false
+	
+
+  win
+end
+
+
+
+def where_is_2 #refacter this whole part
+		
+	where = nil
+	r_d_c = [rows, diagonals, columns]
+	
+	r_d_c.each do |x|
+		if where == nil
+			x.each do |three|
+				if three.count('X') == 2	&& !three.include?('O')
+					index = three.index { |x| x.is_a? Integer } 
+					where = three[index]
+				end	
+			end	
+		end
+
+	end
+	
+  where
+end
+
+
+def rows
+
+	@board
+
+end
+
+
+
+def columns
+	col_1 = []
+	col_2 = []
+	col_3 = []
+	
+	@board.each do |row|
+		col_1 << row[0]
+		col_2 << row[1]
+		col_3 << row[2]
+	end	
+
+	[col_1,col_2,col_3]
+
+end	
+
+
+
+def diagonals #this will just show the diagonals when called, in an array
+	diagonal_1 = [@board[0][0],@board[1][1],@board[2][2]]
+	diagonal_2 = [@board[0][2],@board[1][1],@board[2][0]]
+  
+  [diagonal_1,diagonal_2]
+end
+
+def dots
+	print "Computer's Turn"
+	8.times { print '.'; sleep(0.25)}
+	puts
+end
+
+def play
+		turn = 0
+	while win? == false do 
+		
+		puts "Where would you like to go?"
+		print ">> "
+		move = gets.chomp.to_i
+		player_move(move)
+		show_board
+		turn += 1
+		ai(turn)
+		dots
+		show_board
+	end	
+	puts "You Lose!!"
+end
+
+play
+
+#puts available_spaces
+
 #puts board[row_1.index(8)]					 	
+
+#play
+
+
+##############################################################################
+
+
